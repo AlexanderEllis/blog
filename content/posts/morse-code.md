f167ecdac9f9052d332a0dcffe7ef24b35143caa
Alex Ellis <ellisonjtk@gmail.com>
2022-01-31 23:27:26 -0500

    Rough draft of morse code game with basic functionality

---
+++ content/posts/morse-code.md
@@ -0,0 +1,65 @@
+---
+title: "Morse Code Game"
+date: 2022-01-31T21:50:54-05:00
+draft: true
+---
+
+TODO:
+- First version: hear the dits/dashes, enter the word!
+- 3 game modes: easy, medium, hard
+- Do it so you can play multiple rounds without taking your hands off the keyboard
+
+- Second version: see the word, enter the dits/dashes
+- 3 game modes: easy, medium, hard
+
+<br>
+
+
+
+<h4>
+  Morse Code Game
+</h4>
+
+<span>
+  Press space to play sound.
+</span>
+
+<span>
+  Each dot or dash within a character is followed by period of signal absence, called a space, equal to the dot duration. The letters of a word are separated by a space of duration equal to three dots, and the words are separated by a space equal to seven dots.
+</span>
+
+<br>
+
+
+<br>
+<br>
+
+<h3>Matching Game</h3>
+
+<p>It will play a word, and you have to enter it right.</p>
+<p>Press Enter to see if you got it.</p>
+<p>Press Control to reset your answer and hear it again.</p>
+
+<legend>Please select your preferred difficulty:</legend>
+<div>
+  <input type="radio" id="difficultyEasy"
+    name="difficulty" value="easy" checked>
+  <label for="difficultyEasy">Easy (letters)</label>
+  <input type="radio" id="difficultyMedium" name="difficulty" value="medium" >
+  <label for="difficultyMedium">Medium (words)</label>
+  <input type="radio" id="difficultyHard" name="difficulty" value="hard">
+  <label for="difficultyHard">Hard (sentences)</label>
+</div>
+
+<button id="playGame">Start</button>
+
+<form>
+<div id="matchingGame">
+  <p id="status"></p>
+  <input type="text" id="wordInput" placeholder="Enter message">
+  <br>
+  <button id="submitMessage">Submit</buton>
+</div>
+</form>
+
+<script src="morse-code.js"></script>
---
+++ content/posts/morse-code/morse-code.js
@@ -0,0 +1,296 @@
+
+const FREQUENCY = 440;
+
+const DOT_TIME = 300;
+const DASH_TIME = DOT_TIME * 3;
+const SYMBOL_BREAK = DOT_TIME;
+const LETTER_BREAK = DOT_TIME * 3 * 1.5;
+const WORD_BREAK = DOT_TIME * 7;
+
+
+const MORSE_MAP = {
+  'a': '.-',
+  'b': '-...',
+  'c': '-.-.',
+  'd': '-..',
+  'e': '.',
+  'f': '..-.',
+  'g': '--.',
+  'h': '....',
+  'i': '..',
+  'j': '.---',
+  'k': '-.-',
+  'l': '.-..',
+  'm': '--',
+  'n': '-.',
+  'o': '---',
+  'p': '.--.',
+  'q': '--.-',
+  'r': '.-.',
+  's': '...',
+  't': '-',
+  'u': '..-',
+  'v': '...-',
+  'w': '.--',
+  'x': '-..-',
+  'y': '-.--',
+  'z': '--..',
+  // '1': '.----',
+  // '2': '..---',
+  // '3': '...--',
+  // '4': '....-',
+  // '5': '.....',
+  // '6': '-....',
+  // '7': '--...',
+  // '8': '---..',
+  // '9': '----.',
+  // '0': '-----'
+};
+
+
+const RANDOM_WORDS = [
+  'the',
+  'he',
+  'train',
+  'oh',
+  'arrive',
+  'nine',
+  'PM',
+  'AM',
+  'code',
+  'morse',
+  'tomato',
+  'key',
+  'shipment',
+  'tractor',
+  'grain',
+  'error',
+  'send',
+  'emergency'
+];
+
+
+let note_context;
+let note_node;
+let gain_node;
+
+let audioContextInitialized = false;
+
+function initializeAudioContext() {
+  note_context = new AudioContext();
+  note_node = note_context.createOscillator();
+  gain_node = note_context.createGain();
+  note_node.frequency.value = FREQUENCY.toFixed(2);
+  gain_node.gain.value = 0;
+  note_node.connect(gain_node);
+  gain_node.connect(note_context.destination);
+  note_node.start();
+  audioContextInitialized = true;
+}
+
+function startNotePlaying() {
+  // Pass a start time of 0 so it starts ramping up immediately.
+  gain_node.gain.setTargetAtTime(0.1, 0, 0.001)
+}
+
+function stopNotePlaying() {
+  // Pass a start time of 0 so it starts ramping down immediately.
+  gain_node.gain.setTargetAtTime(0, 0, 0.001)
+}
+
+document.addEventListener('keydown', event => {
+  if (event.repeat || event.keyCode != 32) {
+    return;
+  }
+
+  if (event.keyCode == 32 /* space */) {
+    if (!audioContextInitialized) {
+      initializeAudioContext();
+    }
+    console.log('we got a space down');
+    startNotePlaying();
+  }
+});
+
+document.addEventListener('keyup', event => {
+  if (event.keyCode == 32 /* space */) {
+    console.log('we got a space up');
+    console.log(note_context.currentTime);
+    stopNotePlaying();
+  }
+});
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function playDash() {
+  if (STOPPED) { return; }
+  startNotePlaying();
+  await sleep(DASH_TIME);
+  stopNotePlaying();
+}
+
+async function playDot() {
+  if (STOPPED) { return; }
+  startNotePlaying();
+  await sleep(DOT_TIME);
+  stopNotePlaying();
+}
+
+/**
+ * message is something like '---'
+ */
+async function playLetter(letter) {
+  if (!audioContextInitialized) {
+    initializeAudioContext();
+  }
+  console.log('playing', letter);
+  for (let i = 0; i < letter.length; i++) {
+    if (STOPPED) { return; }
+    if (letter[i] == '-') {
+      await playDash();
+    } else if (letter[i] == '.') {
+      await playDot();
+    }
+    await sleep(SYMBOL_BREAK);
+  }
+}
+
+// Word is an array of letters, like ['.', '.-', '-']
+async function playWord(word) {
+  for (let i = 0; i < word.length; i++) {
+    if (STOPPED) { return; }
+    await playLetter(word[i]);
+    await sleep(LETTER_BREAK);
+  }
+}
+
+var STOPPED = false;
+
+// Sentence is an array of words.
+// Ex. "dog is good" -> [['-..', '---', '--.'], ['..', '...'], ['--.', '---', '---', '-..']]
+async function playSentence(sentence) {
+  STOPPED = false;
+  // Slight pause before it starts
+  await sleep(DASH_TIME);
+  for (let i = 0; i < sentence.length; i++) {
+    await playWord(sentence[i]);
+    await sleep(WORD_BREAK);
+  }
+}
+
+// asciiChar is something like 'd'
+// Assumes [a-z0-9]
+function convertAsciiCharToMorse(asciiChar) {
+  return MORSE_MAP[asciiChar];
+}
+
+// asciiWord is something like 'dog'
+// Assumes [a-z0-9]
+function convertAsciiWordToMorse(asciiWord) {
+  return asciiWord.split('').map(convertAsciiCharToMorse);
+}
+
+// asciiSentence is something like 'dog is good'
+// Assumes words are separated by spaces.
+function convertAsciiSentenceToMorse(asciiSentence) {
+  let splitSentence = asciiSentence.toLowerCase().split(' ');
+  return splitSentence.map(convertAsciiWordToMorse);
+}
+
+function getRandomLetter() {
+  const randomIndex = Math.floor(Math.random() * 26);
+  return Object.keys(MORSE_MAP)[randomIndex];
+}
+
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * RANDOM_WORDS.length);
+  return RANDOM_WORDS[randomIndex];
+}
+
+function getRandomSentence() {
+  return "I have not done this yet."
+}
+
+function getTarget() {
+  let target;
+  // Get the difficulty and assign a random target.
+  const difficulty = document.querySelector('input[name="difficulty"]:checked').value
+  switch (difficulty) {
+    case 'easy':
+      target = getRandomLetter();
+      break;
+    case 'medium':
+      target = getRandomWord();
+      break;
+    default:
+      target = getRandomSentence();
+      break;
+  }
+  return target;
+}
+
+var startButton = document.getElementById('playGame');
+
+// function stop() {
+//   STOPPED = true;
+//   document.getElementById('playGame').textContent = 'Start';
+// }
+
+async function playGame() {
+  let target = getTarget();
+
+  // Update the button text
+  // document.getElementById('playGame').textContent = 'Stop';
+
+  // Status we'll soon update.
+  const status = document.getElementById('status');
+  status.value = 'Waiting...';
+  // Word input we'll be checking
+  const wordInput = document.getElementById('wordInput');
+  wordInput.focus();
+
+  // Async call to play the message.
+  playSentence(convertAsciiSentenceToMorse(target));
+
+  let messageFound = false;
+
+  wordInput.addEventListener('keyup', event => {
+    if (event.key == 'Control') {
+      wordInput.value = '';
+      STOPPED = true;
+      playSentence(convertAsciiSentenceToMorse(target));
+    }
+  });
+
+  // When the submitted button is clicked, we want to check if it's the target.
+  document.getElementById('submitMessage').addEventListener('click', event => {
+    event.preventDefault();
+    if (messageFound) {
+      // If we found it already, get a new target and play it.
+      messageFound = false;
+      wordInput.value = '';
+      status.textContent = 'Waiting...';
+      target = getTarget();
+      playSentence(convertAsciiSentenceToMorse(target));
+    } else {
+      const enteredWord = wordInput.value;
+      if (enteredWord.toLowerCase() == target) {
+        // If they got the word, show message and switch event listener.
+        status.textContent = 'Correct! Press enter to play a new round.'
+        messageFound = true;
+      } else {
+        // If they didn't, clear the text box and let them try again.
+        status.textContent = 'Not quite! Try again.'
+        // Play message.
+        wordInput.value = '';
+        playSentence(convertAsciiSentenceToMorse(target));
+      }
+
+    }
+  })
+}
+
+document.getElementById("playGame").addEventListener('click', playGame);
